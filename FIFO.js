84ffa959d7dd18c97914c3af187438dbcb715c67
Ambika <ambeeka02@gmail.com>
2024-12-29 19:44:57 +0545

    third commit

--- FIFO.js
+++ FIFO.js
@@ -1,18 +1,21 @@
-// fifo.js - FIFO Page Replacement Algorithm
+//  FIFO Page Replacement Algorithm
+// it keeps track of pages in memory
+// it also replace the oldest page when a new page causes a page fault
+
 export function fifoPageReplacement(pages, frameSize) {
-  let frames = [];
-  let pageFaults = 0;
+  let frames = []; //an empty array, which keeps track of pages in memory
+  let pageFault = 0; //counts how many times a page is not found in memory and needs to be loaded.
 
   for (let page of pages) {
     if (!frames.includes(page)) {
       // If the page is not in frames
       if (frames.length === frameSize) {
-        frames.shift(); // Remove the oldest page
+        frames.shift(); // remove the oldest page
       }
       frames.push(page); // Add the new page
-      pageFaults++;
+      pageFault++; //increases the page fault
     }
   }
 
-  return pageFaults;
+  return pageFault; //returns the no of page fault
 }
